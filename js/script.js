bf0bd95419a829ca613e3498e796c7e2b8ecc1ff
TaoistBear <originaltaoistbear@gmail.com>
2016-07-13 17:38:03 -0400

    got it all to work!
    finished project

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 var quotes = [
   {
-    quote: '# 1 : Life is a series of natural and spontaneous changes. Don\'t resist them - that only creates sorrow. Let reality be reality. Let things flow naturally forward in whatever way they like.',
+    quote: 'Life is a series of natural and spontaneous changes. Don\'t resist them - that only creates sorrow. Let reality be reality. Let things flow naturally forward in whatever way they like.',
     source: 'Lao Tzu',
     citation: 'Tao Te Ching',
     year: 'cica 4th century B.C.E.',
@@ -19,7 +19,7 @@ var quotes = [
     category: 'Life'
   },
   {
-    quote: '# 2 : The most important thing is to enjoy your life - to be happy - it\'s all that matters.',
+    quote: 'The most important thing is to enjoy your life - to be happy - it\'s all that matters.',
     source: 'Audry Hepburn',
     citation: 'Audrey Hepburn\'s Enchanted Tales',
     year: 1992,
@@ -30,7 +30,7 @@ var quotes = [
     category: 'Life'
   },
   {
-    quote: '# 3 : We do not remember days, we remember moments',
+    quote: 'We do not remember days, we remember moments',
     source: 'Cesare Pavase',
     citation: 'This Business of Living',
     year: 1940,
@@ -41,7 +41,7 @@ var quotes = [
     category: 'Life'
   },
   {
-    quote: ' # 4 : Imagination will often carry us to worlds that never were. But without it we go nowhere',
+    quote: 'Imagination will often carry us to worlds that never were. But without it we go nowhere',
     source: 'Carl Sagan',
     citation: 'Cosmos',
     year: 1980,
@@ -50,62 +50,62 @@ var quotes = [
     born: 'United States',
     deathYear: 1996,
     category: 'Imagination'
-  }//,
-  // {
-  //   quote: 'Nonsense wakes up the brain cells. And it helps develop a sense of humor, which is awfully important in this day and age. Humor has a tremendous place in this sordid world. It\'s more than just a matter of laughing.',
-  //   source: 'Dr. Seuss',
-  //   citation: 'Author Isn\'t Just a Cat in the Hat: The Los Angeles Times',
-  //   year: 1983,
-  //   profession: 'Author',
-  //   birthYear: 1904,
-  //   born: 'United States',
-  //   deathYear: 1991,
-  //   category: 'Imagination'
-  // },
-  // {
-  //   quote: 'They who dream by day are cognizant of many things which escape those who dream only by night.',
-  //   source: 'Edgar Allen Poe',
-  //   citation: 'Eleonora',
-  //   year: 1841,
-  //   profession: 'Poet',
-  //   birthYear: 1809,
-  //   born: 'United States',
-  //   deathYear: 1849,
-  //   category: 'Imagination'
-  // },
-  // {
-  //   quote: 'No man ever steps in the same river twice, for it\'s not the same river and he\'s not the same man.',
-  //   source: 'Heraclitus',
-  //   citation: 'as quoted by Plato in Cratylus',
-  //   year: 'circa 360 B.C.E.',
-  //   profession: 'Philosopher',
-  //   birthYear: '544 B.C.E.',
-  //   born: 'Greece',
-  //   deathYear: '483 B.C.E.',
-  //   category: 'Experience'
-  // },
-  // {
-  //   quote: 'Be brave. Take risks. Nothing can substitute experience.',
-  //   source: 'Paulo Coelho',
-  //   citation: 'The Alchemist',
-  //   year: 1988,
-  //   profession: 'Novelist',
-  //   birthYear: 1947,
-  //   born: 'Brazil',
-  //   deathYear: '',
-  //   category: 'Experience'
-  // },
-  // {
-  //   quote: 'We shall not grow wiser before we learn that much that we have done was very foolish.',
-  //   source: 'Friedrich August von Hayek',
-  //   citation: 'The Road to Serfdom',
-  //   year: 1944,
-  //   profession: 'Economist',
-  //   birthYear: 1899,
-  //   born: 'Austria',
-  //   deathYear: 1992,
-  //   category: 'Experience'
-  // }
+  },
+  {
+    quote: 'Nonsense wakes up the brain cells. And it helps develop a sense of humor, which is awfully important in this day and age. Humor has a tremendous place in this sordid world. It\'s more than just a matter of laughing.',
+    source: 'Dr. Seuss',
+    citation: 'Author Isn\'t Just a Cat in the Hat: The Los Angeles Times',
+    year: 1983,
+    profession: 'Author',
+    birthYear: 1904,
+    born: 'United States',
+    deathYear: 1991,
+    category: 'Imagination'
+   },
+   {
+    quote: 'They who dream by day are cognizant of many things which escape those who dream only by night.',
+    source: 'Edgar Allen Poe',
+    citation: 'Eleonora',
+    year: 1841,
+    profession: 'Poet',
+    birthYear: 1809,
+    born: 'United States',
+    deathYear: 1849,
+    category: 'Imagination'
+  },
+  {
+    quote: 'No man ever steps in the same river twice, for it\'s not the same river and he\'s not the same man.',
+    source: 'Heraclitus',
+    citation: 'as quoted by Plato in Cratylus',
+    year: 'circa 360 B.C.E.',
+    profession: 'Philosopher',
+    birthYear: '544 B.C.E.',
+    born: 'Greece',
+    deathYear: '483 B.C.E.',
+    category: 'Experience'
+  },
+  {
+    quote: 'Be brave. Take risks. Nothing can substitute experience.',
+    source: 'Paulo Coelho',
+    citation: 'The Alchemist',
+    year: 1988,
+    profession: 'Novelist',
+    birthYear: 1947,
+    born: 'Brazil',
+    deathYear: '',
+    category: 'Experience'
+  },
+  {
+    quote: 'We shall not grow wiser before we learn that much that we have done was very foolish.',
+    source: 'Friedrich August von Hayek',
+    citation: 'The Road to Serfdom',
+    year: 1944,
+    profession: 'Economist',
+    birthYear: 1899,
+    born: 'Austria',
+    deathYear: 1992,
+    category: 'Experience'
+  }
 ];
 
 // set empty array to store used quotes
@@ -113,23 +113,30 @@ var usedQuotes = [];
 
 // SET UP FUNCTION TO RAMDONLY SELECT QUOTES
 function getRandomQuote() {
-  // check to see if both quote arrays are equal.  if they are then empty usedQuote array
-    // to start another cycle
-  if (usedQuotes === quotes) {
-    usedQuotes.splice(0);
-  }
+
   // get index number by mulitplying the random number by quote length
     // keeping it flexible and easily updatable by using the quote.length property
   var quoteIndex = Math.floor(Math.random() * quotes.length)
-  // check to see if quote has already been use
-  while (quotes !== usedQuotes && quotes[quoteIndex] === usedQuotes[quoteIndex]) {
-    var quoteIndex = Math.floor(Math.random() * quotes.length)
-  }
 
   // set a variable to hold the random quote
   var randomQuote = quotes[quoteIndex];
-  // fill empty array with quote alrady generated
-  usedQuotes[quoteIndex] = randomQuote;
+
+  // remove quote from main varialbe to used variable.
+
+  quotes.splice(quoteIndex, 1);
+
+  usedQuotes.push(randomQuote);
+
+  // check to see if quotes array is empty... if so make it equal to used quotes and make that empty
+  if (quotes == '') {
+    quotes = usedQuotes;
+    usedQuotes = [];
+  }
+
+  console.log('used quote array length: ' + usedQuotes.length);
+  console.log('quote array length: ' + quotes.length);
+
+
   // return the random quote
   return randomQuote;
 }
